1cfdc306d813d969e85f8f6b0ab14751e40835eb
Simon Miller <simonmiller17@hotmail.co.uk>
2021-11-18 09:10:10 +0000

    added SOLID (S) user that needs refactoring, and unit tests supporting it.

---
+++ SOLID_SimonMiller.Tests/_1_S/Before/UserEntityTests.cs
@@ -0,0 +1,45 @@
+﻿using SOLID_SimonMiller._1_S.Before;
+
+namespace SOLID_SimonMiller.Tests._1_S.Before
+{
+    [TestClass]
+    public class UserEntityTests
+    {
+        [TestMethod]
+        public void Can_determine_it_exists()
+        {
+            // Arrange
+            var existingUser = UsersFakeContext.Users.First();
+            var notAUser = new UserEntity("unknown", "passwordHashed");
+
+            // purely to demonstrate that a RECORD compares values, NOT memory addresses.
+            var copyOfExistingUser = new UserEntity("a@b.com", "hashedPasswordHere");
+
+            // Act
+            var existsInDb = existingUser.UserExists();
+            var existsInDbToo = copyOfExistingUser.UserExists();
+            var shouldNotExistInDb = notAUser.UserExists();
+
+            // Assert
+            Assert.IsTrue(existsInDb);
+            Assert.IsTrue(existsInDbToo);
+            Assert.IsFalse(shouldNotExistInDb);
+        }
+
+        [TestMethod]
+        public void Can_authenticate()
+        {
+            // Arrange
+            var existingUser = UsersFakeContext.Users.First();
+            var notAUser = new UserEntity("unknown", "passwordHashed");
+
+            // Act
+            var shouldBeAuthenticated = existingUser.IsAuthenticated();
+            var shouldNotBeAuthenticated = notAUser.IsAuthenticated();
+
+            // Assert
+            Assert.IsTrue(shouldBeAuthenticated);
+            Assert.IsFalse(shouldNotBeAuthenticated);
+        }
+    }
+}
---
+++ SOLID_SimonMiller/_1_S/Before/UserEntity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_SimonMiller._1_S.Before
+{
+    /// <summary>
+    /// A user entity would contain information from a repository (database) and could contain a login name (email?) and password (hashed mby your favourite argorithm)
+    /// With 'smart' entities, you often find code on entities allowing them to do things that perhaps should be placed somewhere else...
+    /// </summary>
+    public record class UserEntity(string LoginName, string PasswordHash)
+    {
+        public bool UserExists() => UsersFakeContext.Users.Any(x => x.LoginName.Equals(this.LoginName));
+
+        /// <summary>
+        /// As this is a RECORD, it should compare based on values, not on memory equality.
+        /// See unit tests!
+        /// </summary>
+        public bool IsAuthenticated() => UsersFakeContext.Users.Any(contextUser => contextUser.Equals(this));
+    }
+    
+    public static class UsersFakeContext
+    {
+        public static ICollection<UserEntity> Users { get; set; } = new List<UserEntity> 
+        {
+            new UserEntity("a@b.com", "hashedPasswordHere"),
+            new UserEntity("b@there.com", "35c2ebff598d117b8a32909b0ed77e3a") // RIPEMD-128 Hash
+        };
+    }
+
+}
