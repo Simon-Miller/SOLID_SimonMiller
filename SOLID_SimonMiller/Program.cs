4af52468acae11286a975beab1b1109ac769ac62
Simon Miller <simonmiller17@hotmail.co.uk>
2021-11-18 15:03:06 +0000

    unfinished

--- SOLID_SimonMiller/Program.cs
+++ SOLID_SimonMiller/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+
+using SOLID_SimonMiller._4_I.After;
+
 Console.WriteLine("Hello, World!");
+
+var services = new ServiceCollection();
+
+var inst = new Repository();
+
+services.AddTransient<IUserRepositoryQuery>(svcs => inst); 
+
+
+
+var app = builder.Build();
