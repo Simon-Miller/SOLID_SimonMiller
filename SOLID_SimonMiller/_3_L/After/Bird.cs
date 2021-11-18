b0ba3d3652d8f54092794a62ef66b40c2a755904
Simon Miller <simonmiller17@hotmail.co.uk>
2021-11-18 14:16:26 +0000

    SOLID (L) changed mind - went for a Duck (bird) example, as itr was quicker and easier to develop. Running out of time!

---
+++ SOLID_SimonMiller/_3_L/After/Bird.cs
@@ -0,0 +1,33 @@
+﻿namespace SOLID_SimonMiller._3_L.After
+{
+    public abstract class Bird
+    {
+        public abstract string MakeNoise();
+
+        public abstract string MoveTo(int x, int y);
+    }
+
+    public abstract class LivingBird : Bird
+    {
+        public abstract string Eat(string food);
+    }
+
+    public class Duck : LivingBird
+    {
+        public override string MakeNoise() => "Quack!";
+        public override string MoveTo(int x, int y) => $"Swimms gracefully to x: {x}, y:{y}";
+
+        public override string Eat(string food) => "Waggs tail feathers in appreciation.";
+    }
+
+    public class RemoteControlledRubberDuck : Bird
+    {
+        public override string MakeNoise() => "Squeek!";
+        public override string MoveTo(int x, int y) => $"Paddles irratically to x: {x}, y:{y} thanks to the remote controller.";
+    }
+
+    /*
+     NOTE: Both a Duck and RubberDuck share the Bird base class, and therefore you can swap one Bird for another without undesirable side-effects.
+            Or if you want, both Duck and RubberDuck are completely replaceable by something else that derives from the base class (Bird)
+     */
+}
---
+++ SOLID_SimonMiller/_3_L/Before/Bird.cs
@@ -0,0 +1,28 @@
+﻿namespace SOLID_SimonMiller._3_L.Before
+{
+    public abstract class Bird
+    {
+        public abstract string MakeNoise();
+
+        public abstract string MoveTo(int x, int y);
+
+        public abstract string Eat(string food);
+    }
+
+    public class Duck : Bird
+    {
+        public override string MakeNoise() => "Quack!";
+        public override string MoveTo(int x, int y) => $"Swimms gracefully to x: {x}, y:{y}";
+
+        public override string Eat(string food) => "Waggs tail feathers in appreciation.";
+    }
+
+    public class RubberDuck : Bird
+    {
+        public override string MakeNoise() => "Quack!";
+        public override string MoveTo(int x, int y) => $"Swimms gracefully to x: {x}, y:{y}";
+
+        // undesirable behaviour as a rubber duck is not really a Bird.   Breaking Liskov substitution.
+        public override string Eat(string food) => throw new NotImplementedException();
+    }
+}
